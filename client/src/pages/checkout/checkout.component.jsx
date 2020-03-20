572a0886679795c3362a099181b78e0155254cb5
timefliez1210 <crfabig@gmail.com>
2020-03-20 15:49:47 +0800

    payment infos update

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 import {
   selectCartItems,
   selectCartTotal
-} from '../../redux/cart/cart.selectors';
+} from "../../redux/cart/cart.selectors";
 
 import {
   CheckoutPageContainer,
@@ -16,7 +16,7 @@ import {
   HeaderBlockContainer,
   TotalContainer,
   WarningContainer
-} from './checkout.styles';
+} from "./checkout.styles";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <CheckoutPageContainer>
@@ -44,7 +44,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     <WarningContainer>
       *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
     </WarningContainer>
     <StripeCheckoutButton price={total} />
   </CheckoutPageContainer>
