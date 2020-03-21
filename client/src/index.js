e5c8c12714b558135e9159cba3fea8b0d54a32cc
timefliez1210 <crfabig@gmail.com>
2020-03-21 14:58:40 +0800

    small typo fixed

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store";
-import * as serviceWoker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
 import App from "./App";
